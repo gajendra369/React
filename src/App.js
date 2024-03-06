19f97a74c97757ed5673309976849268f949ed52
gajendra369 <gajendra.20cs028@sode-edu.in>
2024-03-06 15:19:07 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 
 import './App.css';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React , { useState } from 'react';
 
 function App() {
+  const [mode,setMode]=useState('light');
+  const [alert,setAlert]=useState(null)
+
+  const showalert=(msg,typ)=>{
+    setAlert({
+      msg:msg,
+      type:typ
+    }
+    )
+  }
+  const togglemode= () =>{
+    if(mode==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor='grey'
+      showalert(" Dark mode enabled","success")
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor='white'
+      showalert("Success Light mode enabled","success")
+    }
+  }
   return (
     <>
-      <Navbar title="text utils"/>
+      <Navbar title="text utils" mode={mode} togglemode={togglemode}/>
+      <Alert alert={alert}/>
       <div className='container'>
-      <TextForm heading="Enter text to analyze"/>
+      <TextForm heading="Enter text to analyze" mode={mode}/>
       </div>
       
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -10,16 +10,16 @@ export default function TextForm(props) {
         console.log("done")
         setText(event.target.value)
     }
-    const [text,setText]=useState("def")
+    const [text,setText]=useState("")
   return (<>
-        <div>
+        <div style={{color:props.mode==='light'?'black':'white'}}>
             <div className="mb-3">
             <h1>{props.heading}</h1>
-            <textarea className="form-control" id="mybox"value={text} onChange={change} rows="8"></textarea>
+            <textarea className="form-control" id="mybox"value={text} onChange={change} rows="8" style={{color:props.mode==='light'?'black':'white',backgroundColor:props.mode==='light'?'white':'grey'}}></textarea>
             </div>
             <button className="btn btn-primary" onClick={upcase}>Uppercase</button>
         </div>
-        <div>
+        <div style={{color:props.mode==='light'?'black':'white'}}>
             <h2>Your text summary</h2>
             <p>No. of word {text.split(" ").length }, No. of characters {text.length}</p>
         </div>
